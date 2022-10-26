991ee5b1e63ab224b46e9cba4873d7adefbaa120
li <@li:maisiliym.kriom>
2022-10-26 14:56:34 +0700

    (version workingPrototype)

--- flake.nix
+++ flake.nix
@@ -4,8 +4,43 @@
   outputs = { self }: {
     type = "worldFunction";
 
-    function = { zola, stdenv }:
-      ({ src }:
-        stdenv.mkDerivation { };)
-    };
-  }
+    function = { zola, stdenv, kynvyrt }:
+
+      { name ? "unnamedWebsite", src }:
+      let
+        zolaConfig = {
+          base_url = "https://example.com";
+          compile_sass = true;
+          build_search_index = true;
+          markdown =
+            { highlight_code = true; };
+        };
+
+        zolaConfigTomlFile = kynvyrt {
+          neim = "config";
+          valiu = zolaConfig;
+          format = "toml";
+        };
+
+      in
+      stdenv.mkDerivation {
+        inherit name;
+        version = src.shortRev or "unversioned";
+        src = self;
+
+        nativeBuildInputs = [ zola ];
+
+        buildPhase = ''
+          ln -s ${zolaConfigTomlFile} ./config.toml
+          ln -s ${src} ./content
+          zola build
+        '';
+
+        installPhase = ''
+          mkdir -p $out
+          cp -r public/* $out
+        '';
+      };
+
+  };
+}
